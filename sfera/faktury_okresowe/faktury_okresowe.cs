a369f379c8738066c5a89de40e59d86004ff7b72
Przemysław Kwiatkowski <micha@micha.waw.pl>
2026-04-23 09:04:32 +0200

    Poprawna obsługa faktur, zawierających towar oprócz usług.

--- sfera/faktury_okresowe/faktury_okresowe.cs
+++ sfera/faktury_okresowe/faktury_okresowe.cs
@@ -23,6 +23,33 @@ DateTime? ToDate(object value) {
 	}
 }
 
+void SetOrInsertValue(dynamic xml, string elementPath, string value, string[] followingSiblingPaths, string[] precedingSiblingPaths) {
+	if (xml.UstawWartosc(elementPath, value) > 0) return;
+
+	var lastSlash = elementPath.LastIndexOf('/');
+	if (lastSlash <= 0)	return;
+
+	var parentPath = elementPath.Substring(0, lastSlash);
+	var elementName = elementPath.Substring(lastSlash + 1);
+
+	if (followingSiblingPaths != null) foreach (var followingSiblingPath in followingSiblingPaths)	{
+			if (xml.IloscElementow(followingSiblingPath) > 0) {
+				xml.DodajElementRownorzednyPrzed(followingSiblingPath, elementName, value);
+				return;
+			}
+		}
+
+	if (precedingSiblingPaths != null) foreach (var precedingSiblingPath in precedingSiblingPaths)	{
+			if (xml.IloscElementow(precedingSiblingPath) > 0) {
+				xml.DodajElementRownorzednyZa(precedingSiblingPath, elementName, value);
+				return;
+			}
+		}
+
+	xml.DodajElementPodrzedny(parentPath, elementName, value);
+}
+
+
 try {
 	InsERT.SuDokument oDok = (InsERT.SuDokument)Dokument;
 
@@ -35,7 +62,42 @@ try {
 		if (xml.IloscElementow("tns:Faktura/tns:Fa/tns:P_6") > 0) xml.UsunElement("tns:Faktura/tns:Fa/tns:P_6");
 
 		// Dodajemy pole OkresFa
-		xml.DodajElementRownorzednyZa("tns:Faktura/tns:Fa/tns:P_2", "tns:OkresFa", "");
+		SetOrInsertValue(xml, $"tns:Faktura/tns:Fa/tns:OkresFa", "", 
+		new[]
+		{
+			$"tns:Faktura/tns:Fa/tns:P_13_1",
+			$"tns:Faktura/tns:Fa/tns:P_14_1",
+			$"tns:Faktura/tns:Fa/tns:P_14_1W",
+			$"tns:Faktura/tns:Fa/tns:P_13_2",
+			$"tns:Faktura/tns:Fa/tns:P_14_2",
+			$"tns:Faktura/tns:Fa/tns:P_14_2W",
+			$"tns:Faktura/tns:Fa/tns:P_13_3",
+			$"tns:Faktura/tns:Fa/tns:P_14_3",
+			$"tns:Faktura/tns:Fa/tns:P_14_3W",
+			$"tns:Faktura/tns:Fa/tns:P_13_4",
+			$"tns:Faktura/tns:Fa/tns:P_14_4",
+			$"tns:Faktura/tns:Fa/tns:P_14_4W",
+			$"tns:Faktura/tns:Fa/tns:P_13_5",
+			$"tns:Faktura/tns:Fa/tns:P_14_5",
+			$"tns:Faktura/tns:Fa/tns:P_13_6_1",
+			$"tns:Faktura/tns:Fa/tns:P_13_6_2",
+			$"tns:Faktura/tns:Fa/tns:P_13_6_3",
+			$"tns:Faktura/tns:Fa/tns:P_13_7",
+			$"tns:Faktura/tns:Fa/tns:P_13_8",
+			$"tns:Faktura/tns:Fa/tns:P_13_9",
+			$"tns:Faktura/tns:Fa/tns:P_13_10",
+			$"tns:Faktura/tns:Fa/tns:P_13_11",
+			$"tns:Faktura/tns:Fa/tns:P_15",
+			$"tns:Faktura/tns:Fa/tns:KursWalutyZ",
+			$"tns:Faktura/tns:Fa/tns:Adnotacje",
+			$"tns:Faktura/tns:Fa/tns:RodzajFaktury"
+		},
+		new[]
+		{
+			$"tns:Faktura/tns:Fa/tns:WZ",
+			$"tns:Faktura/tns:Fa/tns:P_2"
+		});
+		
 		xml.DodajElementPodrzedny("tns:Faktura/tns:Fa/tns:OkresFa", "tns:P_6_Od", dataOd.Value.ToString("yyyy-MM-dd"));
 		xml.DodajElementRownorzednyZa("tns:Faktura/tns:Fa/tns:OkresFa/tns:P_6_Od", "tns:P_6_Do", dataDo.Value.ToString("yyyy-MM-dd"));
 	}
